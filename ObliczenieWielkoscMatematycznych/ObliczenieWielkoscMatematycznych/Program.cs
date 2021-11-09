d9ca263b0d38fc92a60db0a2b306251e0054800e
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-09 11:38:03 +0100

    create : ObliczenieWielkoscMatematycznych project

---
+++ ObliczenieWielkoscMatematycznych/ObliczenieWielkoscMatematycznych/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObliczenieWielkoscMatematycznych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
